f660d6cb5b33f593a2a97e26355499c991295f1b
Ed <40153785+China-Chris@users.noreply.github.com>
2023-05-18 13:46:15 +0800

    feat:  Initialize Gateway Project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # APIGateway-service
+
+
+开发需要安装 goctl工具
+
+go install github.com/zeromicro/go-zero/tools/goctl@latest
+
+查看版本号
+goctl -version
+goctl version 1.5.2 windows/amd64
+
+
+使用 go-zero提供的便携安装 protoC & protoc-gen-go
+goctl env check -i -f --verbose
+
+生成路由文件
+goctl api go -api gateway.api -dir .
---
+++ gateway/internal/logic/ping/pinglogic.go
@@ -0,0 +1,28 @@
+package ping
+
+import (
+	"context"
+	"fmt"
+
+	"gateway/internal/svc"
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type PingLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewPingLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PingLogic {
+	return &PingLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *PingLogic) Ping() error {
+	fmt.Println("ping")
+	return nil
+}
